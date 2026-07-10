6c45f820cf3d59189fbb0bedf7b02e07cb98a1b1
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2026-07-10 14:00:02 +0900

    [bugfix] fix error in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,5 @@ st.write("""
     左側のメニューから選択してください。
     """)
 
-st.page_link("pages/wavefunction.py", label="wavefunction plotter")
-st.page_link("pages/smiles2svg.py", label="SMILES -> SVG")
+# st.page_link("pages/wavefunction.py", label="wavefunction plotter")
+# st.page_link("pages/smiles2svg.py", label="SMILES -> SVG")
