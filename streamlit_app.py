935b94a668b5898517e47b76a174091891256e27
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2026-07-10 14:07:09 +0900

    [wip]

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,1 @@
-from pathlib import Path
-import streamlit as st
-
-st.set_page_config(page_title="制作室便利ツール", page_icon="🧪")
-
-st.title("制作室便利ツール")
-
-st.write("""
-    左側のメニューから選択してください。
-    """)
-
-st.write("__file__=", __file__)
-st.write("__cwd__=", (Path("pages")).exists())
-st.write("pages exists =", (Path("pages")).exists())
-st.write("pages =", list(Path("pages").glob("*")))
-# st.page_link("pages/wavefunction.py", label="wavefunction plotter")
-# st.page_link("pages/smiles2svg.py", label="SMILES -> SVG")
+Error: Could not open file '/home/yymmt/local/devel/seisakutools/pyproject.toml': Error reading configuration file: Cannot overwrite a value (at line 6, column 27)
