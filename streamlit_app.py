01933649d13a34059d3fdd9536e3a41be0fbdb02
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2026-07-10 14:03:28 +0900

    [wip]

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import streamlit as st
 
 st.set_page_config(page_title="制作室便利ツール", page_icon="🧪")
@@ -8,5 +9,9 @@ st.write("""
     左側のメニューから選択してください。
     """)
 
+st.write("__file__=", __file__)
+st.write("__cwd__=", (Path("pages")).exists())
+st.write("pages exists =", (Path("pages")).exists())
+st.write("pages =", list(Path("pages").glob("*")))
 # st.page_link("pages/wavefunction.py", label="wavefunction plotter")
 # st.page_link("pages/smiles2svg.py", label="SMILES -> SVG")
